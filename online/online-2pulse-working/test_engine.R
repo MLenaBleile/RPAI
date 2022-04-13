3c3220a07f119a1dc952fe790a419e679bf50925
Mlenableile <mbleile@smu.edu>
2022-04-13 10:12:47 -0500

    add new plots

--- online/online-2pulse-working/test_engine.R
+++ online/online-2pulse-working/test_engine.R
@@ -1,18 +1,18 @@
 setwd("C:/Users/s198663/Documents/RPAI/online/online-2pulse-working")
 source("data_generation_fncs.R")
 source("env_fncs_2pulse.R")
-
+set.seed(1998)
 test_num=50
 maxtime = 40
 num_free_pulses=1
-wait_time=7
-potential_actions = 1:13 + wait_time
+wait_time=9
+potential_actions = 1:5 + wait_time
 all.action.mat = make_potential_action_mat(potential_actions = potential_actions, num_free_pulses = num_free_pulses)
 inc_days = 15-2+wait_time
 minibatch_parameters=make_parameter_mat(test_num)
 
 agent.outcomes=rep(NA, test_num)
-reference_days=c(10,14,20)
+reference_days=c(1,potential_actions,20)
 reference.outcomes=matrix(NA, nrow=test_num, ncol=length(reference_days)+1)
 colnames(reference.outcomes) = c(paste("day",reference_days),'random')
 selected_actions=matrix(NA, nrow=test_num, ncol=num_free_pulses)
@@ -23,7 +23,7 @@ optimal_actions = rep(NA,test_num)
 
 for(mouse in 1:test_num){
   parameter_vec= minibatch_parameters[mouse,]
-  one.reference = generate_one_counterfactualset(minibatch_parameters[mouse,],total_days = maxtime, wait_time = 0, potential_actions = potential_actions)
+  one.reference = generate_one_counterfactualset(minibatch_parameters[mouse,],total_days = maxtime, wait_time = 0, potential_actions = 1:20)
   one.optima= which.min(one.reference[,maxtime])+wait_time
   optimal_actions[mouse] = one.optima
   optima.verif[mouse] = get.optim.plan(parameter_vec=parameter_vec, maxtime=maxtime, all.action.mat = all.action.mat)
@@ -32,7 +32,7 @@ for(mouse in 1:test_num){
   for(refday.idx in 1:length(reference_days)){
     reference_day = reference_days[refday.idx]
     #day15.outcomes[mouse] = one.reference[reference_day-6,total_days]
-    reference.outcomes[mouse, refday.idx]=log(one.reference[reference_day-wait_time,maxtime])
+    reference.outcomes[mouse, refday.idx]=log(one.reference[reference_day,maxtime])
   }
   
   random.action = sample(potential_actions, size=num_free_pulses, replace=T)
@@ -54,17 +54,22 @@ reference_actions = c(reference_days, "random")
 effect.sizes = rep(NA, 4)
 
 #x11()
+results = matrix(NA, nrow=ncol(reference.outcomes),ncol=4)
+colnames(results) = c("means","medians", "mins", "maxes")
+rownames(results) = colnames(reference.outcomes)
 par(mfrow=c(2,2))
 colors = c("red","blue","orange","purple")
 plot(density(agent.outcomes-reference.outcomes[,1]),xlim=c(-.05,.2),xlab="fixed final ltv - agent final ltv",type="n", main="2 pulse performance", col="red", ylim=c(0,60))
-for(refday.idx in 1:4){
+for(refday.idx in 1:ncol(reference.outcomes)){
   deltaref = reference.outcomes[,refday.idx]
   lines(density(deltaref - agent.outcomes), col=colors[refday.idx])
   test=t.test(deltaref-agent.outcomes)
   print(test)
+  loss=deltaref-agent.outcomes
   cohen = mean(deltaref-agent.outcomes)/sd(deltaref-agent.outcomes)
   cat("effect size: ", cohen)
   effect.sizes[refday.idx] = cohen
+  results[refday.idx,] = c(mean(loss), median(loss), min(loss), max(loss))
   #cat(agent.outcomes[agent.outcomes>deltaref])
 }
 
@@ -73,6 +78,7 @@ for(refday.idx in 1:4){
 legend("topright", legend=paste(c(paste("day", reference_days), "random"), round(effect.sizes, 3), sep=": d="), pch=16, col=c(colors, "purple"))
 abline(v=0, lty=2)
 
+par(mfrow=c(1,3))
 plot(minibatch_parameters[,'lambda'], est.par.mat[,'lambda'],pch=16, main="lambda", ylab="estimated", xlab="actual")
 plot(minibatch_parameters[,'rho'], est.par.mat[,'rho'], pch=16,main="rho", ylab="estimated", xlab="actual")
 plot(minibatch_parameters[,'mu'], est.par.mat[,'mu'], pch=16,main="mu", ylab="estimated", xlab="actual")
